ed37e73f5140e993dc662e52d434a9525f639e44
anhlieem <leminhhuy06102005@gmail.com>
2025-05-12 00:10:53 +0700

    merge

--- admin/index.php
+++ admin/index.php
@@ -96,11 +96,18 @@ require_once  'Admin-form/Login_Form/Logout/admin_auth.php'; // Chuc nang logout
                 </li>
 
                 <li class="nav-item mb-2">
+<<<<<<< Updated upstream
                     <a class="nav-link text-white d-flex align-items-center gap-2 <?= $currentPage === 'donhang' ? 'active' : '' ?>" href="index.php?page=donhang&pageadmin=1">
                         <i class="fa fa-cart-plus"></i> <span>Đơn hàng</span>
                     </a>
                 </li>
 
+=======
+                    <a class="nav-link text-white d-flex align-items-center gap-2 <?= $currentPage === 'thongke' ? 'active' : '' ?>" href="index.php?page=thongke&pageadmin=1">
+                        <i class="fas fa-chart-line"></i> <span>Thống kê</span>
+                    </a>
+                </li>
+>>>>>>> Stashed changes
 
                 <li class="nav-item mb-2">
                     <a class="nav-link text-white d-flex align-items-center gap-2" href="index.php?action=logout">
@@ -175,8 +182,13 @@ require_once  'Admin-form/Login_Form/Logout/admin_auth.php'; // Chuc nang logout
                     case 'phanquyen':
                         include '../admin/layout/phan_quyen.php';
                         break;
+<<<<<<< Updated upstream
                     case 'donhang':
                         include '../admin/layout/don_hang.php';
+=======
+                    case 'thongke':
+                        include '../admin/layout/thongke.php';
+>>>>>>> Stashed changes
                         break;
                 }
             }
